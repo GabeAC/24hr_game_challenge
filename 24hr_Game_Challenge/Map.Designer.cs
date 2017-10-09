03ae39e15be2c3b23f0c16a315bbfcb862b72cdb
slixxed <1skydran@gmail.com>
2017-10-09 13:17:04 -0400

    added basic scanning for map xml files

--- 24hr_Game_Challenge/Map.Designer.cs
+++ 24hr_Game_Challenge/Map.Designer.cs
@@ -32,8 +32,12 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.screen = new System.Windows.Forms.Panel();
             this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.player1 = new System.Windows.Forms.PictureBox();
             this.screen.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -44,12 +48,14 @@
             // 
             // screen
             // 
-            this.screen.Controls.Add(this.player);
+            this.screen.Controls.Add(this.player1);
+            this.screen.Controls.Add(this.pictureBox1);
             this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
             this.screen.Location = new System.Drawing.Point(0, 0);
             this.screen.Name = "screen";
             this.screen.Size = new System.Drawing.Size(1266, 705);
             this.screen.TabIndex = 0;
+            this.screen.Paint += new System.Windows.Forms.PaintEventHandler(this.screen_Paint);
             // 
             // player
             // 
@@ -60,6 +66,23 @@
             this.player.TabIndex = 0;
             this.player.TabStop = false;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.Black;
+            this.player1.Location = new System.Drawing.Point(486, 475);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(69, 69);
+            this.player1.TabIndex = 1;
+            this.player1.TabStop = false;
+            // 
             // Map
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
@@ -67,22 +90,17 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.YellowGreen;
             this.ClientSize = new System.Drawing.Size(1266, 705);
-
             this.Controls.Add(this.screen);
+            this.Controls.Add(this.player);
             this.Name = "Map";
             this.Text = "Map";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Map_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Map_KeyUp);
             this.screen.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
-
-
-            this.Controls.Add(this.player);
-            this.Name = "Map";
-            this.Text = "24hr Game Challenge";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.Text = "Map";
-
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -91,6 +109,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Panel screen;
         private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- upackmgr/Program.cs
+++ upackmgr/Program.cs
@@ -9,21 +9,11 @@ namespace upackmgr
 {
     class Program
     {
-        private string filename;
 
         static void Main(string[] args)
         {
 
         }
-        private byte[] GetBinaryFile(filename)
-        {
-            byte[] bytes;
-            using (FileStream file = new FileStream(filename, FileMode.Open, FileAccess.Read))
-            {
-                bytes = new byte[file.Length];
-                file.Read(bytes, 0, (int)file.Length);
-            }
-            return bytes;
-        }
+       
     }
 }
