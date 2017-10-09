20dabac320f6e75788ed38362c3ae3a4fc6c9227
kevin116mitchell <kevin116mitchell@gmail.com>
2017-10-09 14:45:27 -0400

    added music and grenade

--- 24hr_Game_Challenge/Map.Designer.cs
+++ 24hr_Game_Challenge/Map.Designer.cs
@@ -30,9 +30,13 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.screen = new System.Windows.Forms.Panel();
+            this.grenade = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.player1 = new System.Windows.Forms.PictureBox();
             this.player = new System.Windows.Forms.PictureBox();
-            this.screen.SuspendLayout();
+            this.grenade.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,14 +46,34 @@
             this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // screen
+            // grenade
             // 
-            this.screen.Controls.Add(this.player);
-            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.screen.Location = new System.Drawing.Point(0, 0);
-            this.screen.Name = "screen";
-            this.screen.Size = new System.Drawing.Size(1266, 705);
-            this.screen.TabIndex = 0;
+            this.grenade.Controls.Add(this.pictureBox1);
+            this.grenade.Controls.Add(this.player1);
+            this.grenade.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grenade.Location = new System.Drawing.Point(0, 0);
+            this.grenade.Name = "grenade";
+            this.grenade.Size = new System.Drawing.Size(1266, 705);
+            this.grenade.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Green;
+            this.pictureBox1.Location = new System.Drawing.Point(259, 357);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(15, 15);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.Black;
+            this.player1.Location = new System.Drawing.Point(219, 343);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(69, 69);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            this.player1.Move += new System.EventHandler(this.player1_Move);
             // 
             // player
             // 
@@ -67,34 +91,26 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.YellowGreen;
             this.ClientSize = new System.Drawing.Size(1266, 705);
-<<<<<<< HEAD:24hr_Game_Challenge/Form1.Designer.cs
-            this.Controls.Add(this.screen);
+            this.Controls.Add(this.grenade);
+            this.Controls.Add(this.player);
             this.Name = "Map";
             this.Text = "Map";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Map_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Map_KeyUp);
-            this.screen.ResumeLayout(false);
+            this.grenade.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
-=======
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel5);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.player);
-            this.Name = "Map";
-            this.Text = "24hr Game Challenge";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.Text = "Map";
->>>>>>> d378c6029ec26a13b0c974898ab798d2f088141e:24hr_Game_Challenge/Map.Designer.cs
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Panel screen;
         private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        protected System.Windows.Forms.Panel grenade;
     }
 }
 
--- 24hr_Game_Challenge/Map.cs
+++ 24hr_Game_Challenge/Map.cs
@@ -7,51 +7,121 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WMPLib; 
 
 namespace _24hr_Game_Challenge
 {
     public partial class Map : Form
     {
+        WindowsMediaPlayer musicPlayer = new WindowsMediaPlayer();
 
+//Glorious Variables----------------------------------------------------------------------------------------------------------------------------------
+        bool right, left;
+        bool jump;
+        int G = 40;
+        int force;
+        Point temp;
 
-       bool right, left;
-       
-        //bool jump;
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 
         public Map()
         {
             InitializeComponent();
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-<<<<<<< HEAD:24hr_Game_Challenge/Form1.cs
-            if (right == true) {player.Left += 5; }
-            if (left == true) { player.Left -= 5; }
-        }
-
-      
-
-        private void Map_KeyDown(object sender, KeyEventArgs e)
-        {
-            if(e.KeyCode == Keys.Right) { right = true; }
-            if(e.KeyCode == Keys.Left) { left = true; }
-=======
-           
+            musicPlayer.URL = "Behelit.m4a";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
->>>>>>> d378c6029ec26a13b0c974898ab798d2f088141e:24hr_Game_Challenge/Map.cs
+            musicPlayer.controls.play();
         }
 
+
+
+        //Player movements-------------------------------------------------------------------------------------------------------------------------------------
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Keep grenade on player
+           // grenade.Location;
+
+
+
+            //Move Right
+            if (right == true) {player1.Left += 5; }
+
+            //Move Left
+            if (left == true) { player1.Left -= 5; }
+
+            //Jump
+            if (jump == true)
+            {
+                player1.Top -= force;
+                force -= 1;
+            }
+            if (player1.Top + player1.Height >= grenade.Height)
+            {
+                player1.Top = grenade.Height - player1.Height;
+                jump = false;
+            }
+            else
+            {
+                player1.Top += 20;
+            }
+
+            //****************Character Actions************************
+
+
+            //*********************************************************
+
+
+        }
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+        //KeyDown Events---------------------------------------------------------------------------------------------------------------------------------------
+        private void Map_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.D || e.KeyCode == Keys.Right) { right = true; }
+            if(e.KeyCode == Keys.A || e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space || e.KeyCode == Keys.W)
+                {
+                    jump = true;
+                    force = G; 
+                }
+            }  
+        }
+        
+        private void player1_Move(object sender, EventArgs e)
+        {
+
+        }
+        
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+        //Keyup Events-----------------------------------------------------------------------------------------------------------------------------------------
         private void Map_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Right) { right = false; }
-            if (e.KeyCode == Keys.Left) { left = false; }
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left) { left = false; }
         }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
 
 
 
     }
 }
+
+
